6b0fa2efad012d063e4a1e2a46e8be29469b19e4
tbs <mjczyt@qq.com>
2016-12-29 16:04:17 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const cmd = require("child_process").exec;
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { classTable: 'Express', grade: "89" });
+
+});
+router.post("/login", function (req, res, next) {
+  console.log(req.body);
+  cmd("node login " + req.body.user + " " + req.body.password + " " + req.body.week, function (err, stdout, stderr) {
+    var content = JSON.parse(stdout);
+    res.render('user', { name:content.stuInfo.studentName,id:content.stuInfo.studentId,classTable: content.classTable, grade: content.grade});
+
+
+  });
+
+
+})
+
+module.exports = router;
