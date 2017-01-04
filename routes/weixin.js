ddefeae68f42e17e65f3c5cc1bbbd47f9e589f80
tbs <mjczyt@qq.com>
2017-01-04 20:57:22 +0800

    weixin1

--- routes/weixin.js
+++ routes/weixin.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  console.log(req.param("signature"));
+if(req.query.signature=="2b572620057cb9c2a47c0639c88001c61edbf9ab"){
+  res.send(req.query.echostr);
+}else{console.log(req.query.signature);}
 });
 
 module.exports = router;
