aabcec7def83a9dd644dac309f2f0c8c032657c8
tbs <mjczyt@qq.com>
2017-01-04 20:41:29 +0800

    add weixin

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,18 @@ const cmd = require("child_process").exec;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { classTable: 'Express', grade: "89" });
+  res.render('index');
 
 });
 router.post("/login", function (req, res, next) {
   console.log(req.body);
   cmd("node login " + req.body.user + " " + req.body.password + " " + req.body.week, function (err, stdout, stderr) {
     var content = JSON.parse(stdout);
-    res.render('user', { name:content.stuInfo.studentName,id:content.stuInfo.studentId,classTable: content.classTable, grade: content.grade});
-
-
+    res.render('user', { name: content.stuInfo.studentName, id: content.stuInfo.studentId, classTable: content.classTable, grade: content.grade });
   });
-
+  router.get("/weixin", function () {
+    console.log(req.param("signature"));
+  })
 
 })
 
