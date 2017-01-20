e6db41753141a8872b2c5b81566cd3692c4f5bc2
tbs <mjczyt@qq.com>
2017-01-20 10:07:21 +0800

    change css

--- routes/basic.js
+++
@@ -1,6 +0,0 @@
-var webshot = require('../lib/webshot');
-
-webshot('http://cqyou.top:2000/class/20145074/RE1MNTQ2Nw==', './amazon.png', function(err) {
-  if (err) return console.log(err);
-  console.log('OK');
-});
