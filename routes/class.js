f07f490f192825f8e4d9a58255652bb795d10313
tbs <mjczyt@qq.com>
2017-01-20 10:29:01 +0800

    set photo

--- routes/class.js
+++ routes/class.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
+var webshot = require("../public/javascripts/webshot")
+    /* GET users listing. */
 router.get('/:id/:pwd', function(req, res, next) {
     var pwd = new Buffer(req.params.pwd, 'base64').toString()
- 	var info={
-    	id:req.params.id,
-    	pwd:pwd
+    var info = {
+        id: req.params.id,
+        pwd: pwd
     };
-    res.render('class',{info:info});
+    res.render('class', { info: info });
 });
 
+router.post('/photo', function(req, res, next) {
+    webshot(req.body.url, './public/webshot/'+req.body.id+'.jpg', function(err) {
+        if (err) return console.log(err);
+        res.end("ok");
+        console.log('OK');
+    });
+})
 
 
 module.exports = router;
