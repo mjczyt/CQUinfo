12d82f6e5ed38c15a620bb96276c7fd89712feaa
tbs <mjczyt@qq.com>
2017-02-24 23:45:31 +0800

    test

--- routes/grade.js
+++ routes/grade.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var config = require('.././config')
+var model = require("./weixinModel");
+var studentModel = require("./studentModel");
+
 
 /* GET users listing. */
-router.get('/:id/:pwd', function(req, res, next) {
+router.get('/:openid', function(req, res, next) {
     var info = {
-        id: req.params.id,
-        pwd: req.params.pwd,
+        openid: req.params.openid,
         mainSite: config.mainSite
     };
-    res.render('grade', { info: info, grade: str.grade });
+    model.findOne({ openid: req.params.openid }, function(err, std) {
+        if (std) {
+            studentModel.findOne({ openid: req.params.openid }, function(error, studentInfo) {
+                res.render('grade', { info: info, grade: studentInfo.grade });
+            })
 
+        } else {
+            console.log("跳转至绑定页面");
+        }
+    })
 });
-
-var str = { "grade": ["毛泽东思想和中国特色社会主义理论体系概论 : 78.00", "形势与政策（3 : 95.00", "线性代数 : 77.00", "大学物理Ⅱ : 66.00", "陈述与沟通 : 81.00", "大学生心理健康与成长 : 良好", "电子实习（Ⅱ） : 良好", "工程测量实习 : 优秀", "毛泽东思想和中国特色社会主义理论体系概论实践 : 未录入", "理论力学（Ⅲ : 88.00", "电工学原理（Ⅱ） : 84.00", "工程测量（I : 80.00", "体育自选项目 : 未录入"], "used-time": 1225 }
 
 
 module.exports = router;
