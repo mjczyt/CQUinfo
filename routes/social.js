e7b3b8a311c2f8539783ebf1ee9e4f84b46aaf1e
tbs <mjczyt@qq.com>
2017-02-25 18:58:24 +0800

    test

--- routes/social.js
+++ routes/social.js
@@ -25,6 +25,53 @@ router.get('/:openid', function(req, res, next) {
     })
 });
 
+router.get('/street/:openid', function(req, res, next) {
+    var info = {
+        openid: req.params.openid,
+        mainSite: config.mainSite
+    };
+    model.findOne({ openid: req.params.openid }, function(err, std) {
+        if (std) {
+            studentModel.findOne({ openid: req.params.openid }, function(error, studentInfo) {
+                res.render('street', { info: info });
+            })
 
+        } else {
+            console.log("跳转至绑定页面");
+        }
+    })
+});
+router.get('/message/:openid', function(req, res, next) {
+    var info = {
+        openid: req.params.openid,
+        mainSite: config.mainSite
+    };
+    model.findOne({ openid: req.params.openid }, function(err, std) {
+        if (std) {
+            studentModel.findOne({ openid: req.params.openid }, function(error, studentInfo) {
+                res.render('message', { info: info });
+            })
+
+        } else {
+            console.log("跳转至绑定页面");
+        }
+    })
+});
+router.get('/chat/:openid', function(req, res, next) {
+    var info = {
+        openid: req.params.openid,
+        mainSite: config.mainSite
+    };
+    model.findOne({ openid: req.params.openid }, function(err, std) {
+        if (std) {
+            studentModel.findOne({ openid: req.params.openid }, function(error, studentInfo) {
+                res.render('chat', { info: info });
+            })
+
+        } else {
+            console.log("跳转至绑定页面");
+        }
+    })
+});
 
 module.exports = router;
